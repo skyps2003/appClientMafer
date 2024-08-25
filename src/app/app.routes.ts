2615a484900abd76cc036e06e6c77e008eaa4239
luis <114497422+skyps2003@users.noreply.github.com>
2024-08-25 13:51:37 -0500

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingLayoutComponent } from './admin/landing-layout/landing-layout.component';
 import { AuthGuard } from './core/guards/auth.guard';
+import { ContactUsComponent } from './shared/components/contact-us/contact-us.component'; 
+import { ShippingPoliciesComponent } from './shared/components/shipping-policies/shipping-policies.component';
+import { UsagePoliciesComponent } from './shared/components/usage-policies/usage-policies.component'; // Importar UsagePoliciesComponent
 
 export const routes: Routes = [
     {
@@ -13,6 +16,18 @@ export const routes: Routes = [
     },
     {
         path: 'admin',
-        loadChildren: () => import('./admin/admin.routes').then(m => m.ADMIN_ROUTES), canActivate: [AuthGuard]
+        loadChildren: () => import('./admin/admin.routes').then(m => m.ADMIN_ROUTES), 
+    },
+    {
+        path: 'contact',
+        component: ContactUsComponent
+    },
+    {
+        path: 'shipping-policies',
+        component: ShippingPoliciesComponent
+    },
+    {
+        path: 'usage-policies', // Nueva ruta para UsagePoliciesComponent
+        component: UsagePoliciesComponent
     }
 ];
